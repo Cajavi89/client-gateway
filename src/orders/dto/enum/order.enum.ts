a020b3852e29cb1f5ac3a4d0a87c0ce355c13c3c
Carlos Jaramillo <ajaramillo@magnetoglobal.com>
2026-03-04 22:50:12 -0500

    config

--- src/orders/dto/enum/order.enum.ts
+++ src/orders/dto/enum/order.enum.ts
@@ -1,10 +1,12 @@
 export enum OrderStatus {
   PENDING = 'PENDING',
+  PAID = 'PAID',
   DELIVERED = 'DELIVERED',
   CANCELED = 'CANCELED',
 }
 
 export const OrderStatusList = [
+  OrderStatus.PAID,
   OrderStatus.PENDING,
   OrderStatus.DELIVERED,
   OrderStatus.CANCELED,
