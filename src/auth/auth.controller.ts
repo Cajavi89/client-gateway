78606b5b9af015e1408e9fa4004f093c851c6fe6
Carlos Jaramillo <ajaramillo@magnetoglobal.com>
2026-03-05 09:35:18 -0500

    implementacion auth micro service

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config/services';
+
+@Controller('auth')
+export class AuthController {
+  constructor(@Inject(NATS_SERVICE) private readonly natsClient: ClientProxy) {}
+
+  @Post('register')
+  registerUser() {
+    return this.natsClient.send('auth.register.user', {});
+  }
+
+  @Post('login')
+  loginUser() {
+    return this.natsClient.send('auth.login.user', {});
+  }
+
+  @Get('verify')
+  verifyUser() {
+    return this.natsClient.send('auth.verify.user', {});
+  }
+}
